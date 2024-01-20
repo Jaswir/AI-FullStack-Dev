ad10ce0cfd63e36911c1c970ad9e5abb976ab7ef
Jaswir <jaswirraghoe@gmail.com>
2024-01-20 21:20:16 +0100

    Remove initial website

--- my_website/script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    var menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
-}
-
-var menuToggle = document.querySelector('.menu-toggle');
-menuToggle.style.display = 'block';
